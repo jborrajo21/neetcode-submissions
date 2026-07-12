3479138ba4a85f1d066870981bb08351bf6defb4
Javier Borrajo Fruguglietti <126285975+jborrajo21@users.noreply.github.com>
2026-07-12 22:57:58 +0100

    Add: balanced-binary-tree - submission-2

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-2.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = True
+        
+        def dfs(root):
+            nonlocal balanced
+            if not root: return 0
+            l,r = dfs(root.left), dfs(root.right)
+            balanced = abs(l - r) <= 1 if balanced else False
+            print(root.val, balanced, l, r)
+            return 1 + max(l, r)
+        
+        dfs(root)
+        return balanced
