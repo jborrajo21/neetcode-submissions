0fd0c349165f0c0dd84e2c486a6d8a4ddfdaf123
Javier Borrajo <126285975+jborrajo21@users.noreply.github.com>
2026-07-28 18:27:32 -0500

    Add: design-twitter-feed - submission-9

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-9.py
@@ -0,0 +1,40 @@
+from collections import defaultdict
+class Twitter:
+
+    def __init__(self):
+        self.follows = defaultdict(set)
+        self.posts = defaultdict(list)
+        self.time = 0
+        
+
+    def postTweet(self, userId: int, tweetId: int) -> None:
+        self.posts[userId].append((tweetId, userId, self.time, len(self.posts[userId])))
+        self.time += 1
+
+    def getNewsFeed(self, userId: int) -> List[int]:
+        feed = []
+        self.follows[userId].add(userId)
+        heap = []
+        heapq.heapify(heap)
+
+        for followee in self.follows[userId]:
+            if self.posts[followee]:
+                tweet, user, time, idx = self.posts[followee][-1]
+                heapq.heappush(heap, (-time, user, tweet, idx))
+
+        while heap and len(feed) < 10:
+            time, user, tweet, idx = heapq.heappop(heap) 
+            feed.append(tweet)
+            if idx > 0:
+                tweet, poster, time, idx1 = self.posts[user][idx-1]
+                heapq.heappush(heap, (-time, poster, tweet, idx1))
+
+        return feed            
+        
+
+    def follow(self, followerId: int, followeeId: int) -> None:
+        self.follows[followerId].add(followeeId)
+
+    def unfollow(self, followerId: int, followeeId: int) -> None:
+        if followeeId in self.follows[followerId]:
+            self.follows[followerId].remove(followeeId)
