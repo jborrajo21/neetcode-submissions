f3bad017b2d9bd7b646b2aa511b7abe9878c36c9
Javier Borrajo <126285975+jborrajo21@users.noreply.github.com>
2026-08-17 16:18:39 -0500

    Add: islands-and-treasure - submission-0

---
+++ Data Structures & Algorithms/islands-and-treasure/submission-0.py
@@ -0,0 +1,28 @@
+class Solution:
+    def islandsAndTreasure(self, grid: List[List[int]]) -> None:
+        INF = 2**31 - 1
+        seen = set()
+
+        def bfs(i, j):
+            visited.add((i,j))
+            q = deque([(i,j,1)])
+            while q: 
+                x,y,d = q.popleft()
+                for dx, dy in [(1,0), (0,1), (-1,0), (0,-1)]:
+                    nx,ny = x + dx, y + dy
+                    if 0<=nx<len(grid) and 0<=ny<len(grid[0]) and grid[nx][ny] > 0 and (nx,ny) not in visited and d < grid[nx][ny]:
+                        grid[nx][ny] = d
+                        visited.add((nx,ny))
+                        q.append((nx,ny, d + 1))
+
+            
+
+        for i in range(len(grid)):
+            for j in range(len(grid[0])):
+                if grid[i][j] == 0 and (i,j) not in seen:
+                    visited = set()
+                    seen.add((i,j))
+                    bfs(i, j)
+
+
+        
