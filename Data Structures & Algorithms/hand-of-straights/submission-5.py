5403e7d8aa8afdaf0a8fab7fd90d543dbe9e20e7
Javier Borrajo <126285975+jborrajo21@users.noreply.github.com>
2026-09-12 17:26:11 -0500

    Add: hand-of-straights - submission-5

---
+++ Data Structures & Algorithms/hand-of-straights/submission-5.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isNStraightHand(self, hand: List[int], groupSize: int) -> bool:
+        if len(hand) % groupSize != 0:
+            return False
+        
+        freqs = {}
+        for n in hand:
+            freqs[n] = freqs.get(n,0) + 1
+        
+        for n in sorted(freqs):
+            num = freqs[n]
+            if num > 0:
+                for i in range(n, n+groupSize):
+                    if i in freqs and freqs[i] >= num:
+                        freqs[i]-=num
+                    else:
+                        return False
+
+        return True
