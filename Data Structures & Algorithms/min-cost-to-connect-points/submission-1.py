b7d6823f37f45386c182bb60d5f4eec927fd3dba
Javier Borrajo <126285975+jborrajo21@users.noreply.github.com>
2026-09-15 11:51:41 -0500

    Add: min-cost-to-connect-points - submission-1

---
+++ Data Structures & Algorithms/min-cost-to-connect-points/submission-1.py
@@ -0,0 +1,41 @@
+class Solution:
+    def minCostConnectPoints(self, points: List[List[int]]) -> int:
+        g = [i for i in range(len(points))] 
+        sizes = [1 for _ in range(len(points))] 
+
+        def root(v):
+            i = v
+            while i != g[i]:
+                i = g[g[i]]
+            return i
+        
+        def connected(u,v):
+            return root(u) == root(v)
+        
+        def union(u,v):
+            r1,r2 = root(u), root(v)
+            if sizes[r1] > sizes[r2]:
+                g[r2] = r1
+                sizes[r1] += sizes[r2]
+            else:
+                g[r1] = r2
+                sizes[r2] += sizes[r1]
+        
+        
+        edges = []
+        for i in range(len(points) - 1):
+            for j in range(i+1, len(points)):
+                edges.append((abs(points[i][0] - points[j][0]) + abs(points[i][1] - points[j][1]), i, j))
+        
+        edges.sort()
+        mst = 0
+        count = 0
+        for cost, u,v in edges:
+            if not connected(u,v):
+                mst += cost
+                union(u,v)
+                count += 1
+                if count == len(points) - 1:
+                    break
+
+        return mst 
